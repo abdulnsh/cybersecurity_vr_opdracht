47f61fd97c13cbf0d2fe1a0da0ec4fcd5f355cc5
defredemi <demi.defre@student.hogent.be>
2024-05-13 18:46:09 +0200

    kali.sh

--- automatisatie_scripts/kali.sh
+++ automatisatie_scripts/kali.sh
@@ -7,13 +7,13 @@ VM_NAME="Kali"
 # vm aanmaken
 VBoxManage createvm --name "$VM_NAME" --ostype "Debian_64" --register --basefolder "D:/HOGENT/TI Netwerken/Semester 2/Cybersecurity & Virtualisation"
 
-VBoxManage modifyvm "Kali_Linux_VM" --memory 4096 --cpus 2
-VBoxManage storagectl "Kali_Linux_VM" --name "SATA Controller" --add sata --controller IntelAhci
-VBoxManage storageattach "Kali_Linux_VM" --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "$VDI"
+VBoxManage modifyvm "$VM_NAME" --memory 4096 --cpus 2
+VBoxManage storagectl "$VM_NAME" --name "SATA Controller" --add sata --controller IntelAhci
+VBoxManage storageattach "$VM_NAME" --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "$VDI"
 
 # bridged adapter
 VBoxManage modifyvm "$VM_NAME" --nic1 bridged
 
 # vm opstarten
-VBoxManage startvm "Kali_Linux_VM"
+VBoxManage startvm "$VM_NAME"
 
