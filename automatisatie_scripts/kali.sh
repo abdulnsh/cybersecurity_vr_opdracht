fec2a9bf2fee84cbb90a68d22d3f86bb11e0d88d
defredemi <demi.defre@student.hogent.be>
2024-05-13 18:27:55 +0200

    ip adressen

--- automatisatie_scripts/kali.sh
+++ automatisatie_scripts/kali.sh
@@ -3,6 +3,7 @@
 # variabelen
 VDI="D:\HOGENT\TI Netwerken\Semester 2\Cybersecurity & Virtualisation\Kali Linux 2023.4 (64bit).vdi"
 VM_NAME="Kali"
+IP_ADDRESS="192.168.1.17"
 
 # vm aanmaken
 VBoxManage createvm --name "$VM_NAME" --ostype "Debian_64" --register --basefolder "D:/HOGENT/TI Netwerken/Semester 2/Cybersecurity & Virtualisation"
@@ -10,6 +11,9 @@ VBoxManage createvm --name "$VM_NAME" --ostype "Debian_64" --register --basefold
 VBoxManage modifyvm "Kali_Linux_VM" --memory 4096 --cpus 2
 VBoxManage storagectl "Kali_Linux_VM" --name "SATA Controller" --add sata --controller IntelAhci
 VBoxManage storageattach "Kali_Linux_VM" --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "$VDI"
+
+# ip adres 
+VBoxManage guestproperty set "$VM_NAME" "/VirtualBox/GuestInfo/Net/0/V4/IP" "$IP_ADDRESS"
 
 # bridged adapter
 VBoxManage modifyvm "$VM_NAME" --nic1 bridged
--- automatisatie_scripts/windows1909.sh
+++ automatisatie_scripts/windows1909.sh
@@ -3,6 +3,7 @@
 # variabelen
 ISO_PATH="D:/HOGENT/TI Netwerken/Semester 2/Cybersecurity & Virtualisation/SW_DVD9_Win_Pro_10_20H2.10_64BIT_English_Pro_Ent_EDU_N_MLF_X22-76585.ISO"
 VM_NAME="Windows10"
+IP_ADDRESS="192.168.1.20"
 
 # vm aanmaken
 VBoxManage createvm --name "$VM_NAME" --ostype "Windows10_64" --register --basefolder "D:/HOGENT/TI Netwerken/Semester 2/Cybersecurity & Virtualisation"
@@ -10,6 +11,9 @@ VBoxManage modifyvm "$VM_NAME" --memory 4096 --cpus 2
 
 VBoxManage storagectl "$VM_NAME" --name "SATA Controller" --add sata --controller IntelAhci
 VBoxManage storageattach "$VM_NAME" --storagectl "SATA Controller" --port 0 --device 0 --type dvddrive --medium "$ISO_PATH"
+
+# ip adres 
+VBoxManage guestproperty set "$VM_NAME" "/VirtualBox/GuestInfo/Net/0/V4/IP" "$IP_ADDRESS"
 
 # bridged adapter
 VBoxManage modifyvm "$VM_NAME" --nic1 bridged
